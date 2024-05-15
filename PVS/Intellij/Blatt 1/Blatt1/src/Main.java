e33add1c15eb0829da8f87f2ec550817d5d7c746
Daniele Vella <daniele.vella@uni-ulm.de>
2024-05-15 15:01:33 +0200

    general_PVS_1

--- PVS/Intellij/Blatt 1/Blatt1/src/Main.java
+++ PVS/Intellij/Blatt 1/Blatt1/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        switch(2) {
+            case 0: System.out.print("A"); break;
+            case 1:
+            case 2: System.out.print("B");
+            case 3: System.out.print("C");
+            default: System.out.print("default");
+        }
     }
 }
