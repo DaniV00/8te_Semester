7aa84915d3c17a3f04d5dd7edb47942222886632
Daniele Vella <daniele.vella@uni-ulm.de>
2024-06-03 14:14:30 +0200

    pVS_Push

--- PVS/Intellij/Blatt__6/src/main/java/Main.java
+++ PVS/Intellij/Blatt__6/src/main/java/Main.java
@@ -1,7 +1,22 @@
 package main.java;
 
+import main.java.exercise2.*;
+//import main.java.exercise2.Student;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Student dani = new Student("1","Dani",24);
+        Student ram = new Student("2","Ram",25);
+        Course PvS = new Course("01","PvS");
+        System.out.println(dani.enrollCourse(PvS));
+        System.out.println(ram.enrollCourse(PvS));
+        System.out.println(dani.leaveCourse(PvS));
+        System.out.println(ram.isEnrolled());
+
     }
+
+
+
 }
--- PVS/Intellij/Blatt__6/src/main/java/exercise2/Student.java
+++ PVS/Intellij/Blatt__6/src/main/java/exercise2/Student.java
@@ -1,13 +1,37 @@
 package main.java.exercise2;
 
+import java.util.HashMap;
+
 public class Student {
     private final String id;
     private final String name;
+    private HashMap <String, Course> hashCourse = new HashMap<>();
 
     public Student(String id, String name, int age) {
         this.id = id;
         this.name = name;
     }
+
+    public String enrollCourse(Course course){
+        hashCourse.put(this.id,course);
+        String currentcourse = ""+ course.getCourseName();
+
+        return "The student with id " + this.id + " and name "+ this.name + " has been enrolled into the course " + currentcourse ;
+    }
+
+    public boolean isEnrolled(){
+        return hashCourse.containsKey(this.id);
+
+    }
+
+    public String leaveCourse(Course course){
+        hashCourse.remove(this.id,course);
+        String currentcourse = "" + course.getCourseName();
+
+        return "The student with id " + this.id + " and name "+ this.name + " has left the course " + currentcourse ;
+    }
+
+
 
     public String getId() {
         return id;
