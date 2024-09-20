372162c069bda9cadf885f1e68343d131d24405e
Daniele Vella <daniele.vella@uni-ulm.de>
2024-09-20 17:17:31 +0200

    wtf lol

--- PVS_EXC_2teVers/INTELLIJ/7/sheet-7-submission/src/main/java/Main.java
+++ PVS_EXC_2teVers/INTELLIJ/7/sheet-7-submission/src/main/java/Main.java
@@ -1,20 +1,48 @@
 import exercise2.Book;
 import exercise2.Library;
+import exercise3.ValuableItem;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 
     public static int sumOfIntegers(List<Integer> list) {
+
         return list.stream().mapToInt(Integer::intValue).sum();
     }
 
     public static String concaten(List<String> listString) {
 
-        return listString.stream().reduce("",String::concat);
+        return listString.stream().reduce("", (s1, s2) -> s1 + s2);
 
     }
+
+    public static Integer findMin (List<ValuableItem> listval){
+
+        return listval.stream().min(Comparator.comparing(ValuableItem::price)).map(ValuableItem::price).orElse(null);
+
+    }
+    public static double findAvg (List<ValuableItem> listavg){
+
+        return listavg.stream().mapToDouble(ValuableItem::price).average().getAsDouble();
+
+    }
+
+    public static double findMedian (List<Integer> listval){
+
+        List<Integer> sortedlist = listval.stream().sorted().toList();
+
+        if (sortedlist.size()/2==1){
+            return sortedlist.get(sortedlist.size()/2);
+        }
+        else {
+            return (sortedlist.get(sortedlist.size()/2-1) + sortedlist.get(sortedlist.size()/2))/2.0;
+        }
+
+    }
+
 
     public static void main(String[] args) {
 
@@ -28,11 +56,19 @@ public class Main {
         List listString = new ArrayList();
         listString.add("C");
         listString.add("IA");
-        listString.add("O");
+        listString.add("LEL");
+
+        List<ValuableItem> items = List.of(
+                new ValuableItem("aa",5,"apple"),
+                new ValuableItem("Item2", 5,"nanas"));
 
 
         //System.out.println(sumOfIntegers(list));
-        System.out.println(concaten(listString));
+        //System.out.println(concaten(listString));
+        System.out.println((findMin(items)));
+        System.out.println((findAvg(items)));
+        System.out.println((findMedian(list)));
+
 
 
 
